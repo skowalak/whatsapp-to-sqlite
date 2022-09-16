46bf8bf2eafa3204c687b4b9e6e1c47684dd46fa
Sebastian Kowalak <sebastian@skowalak.de>
2022-09-16 22:00:06 +0200

    feat(cli): ✨ restructure click interface

--- whatsapp_to_sqlite/cli.py
+++ whatsapp_to_sqlite/cli.py
@@ -1,65 +1,118 @@
 import datetime
-import os
 import logging
-import sqlite_utils
+import os
+import pathlib
+import shutil
 
 import click
+import sqlite_utils
 
 from whatsapp_to_sqlite import utils
-
-LOG = logging.getLogger(__name__)
 
 
 @click.group()
 @click.version_option()
 def cli():
-    """Convert your exported plaintext message logs to an SQLite database."""
+    """
+    Convert your exported plaintext message logs to an SQLite database.
+    """
 
 
-@cli.command()
+@cli.command(name="import")
 @click.argument(
-    "db_path",
-    type=click.Path(file_okay=True, dir_okay=False, allow_dash=False),
+    "chat_files",
+    type=click.Path(file_okay=True, dir_okay=True, allow_dash=True),
     required=True,
 )
 @click.argument(
-    "log_directory",
-    type=click.Path(file_okay=False, dir_okay=True, allow_dash=True),
-    required=True,
+    "db_path",
+    default="messagedb.sqlite3",
+    type=click.Path(file_okay=True, dir_okay=False, allow_dash=False),
+    required=False,
 )
 @click.option(
     "-d",
     "--data-directory",
     type=click.Path(file_okay=False, dir_okay=True, allow_dash=False),
-    default="data",
-    help="Path(s) which contain additional exported media",
+    help=(
+        "Path(s) which contain additional exported media. "
+        "If left empty images, videos, files and voice memos "
+        "will not be processed."
+    ),
+    required=False,
+)
+@click.option(
+    "-o",
+    "--output-directory",
+    default="messagedb_files",
+    type=click.Path(file_okay=False, dir_okay=True, allow_dash=False),
+    help=("Path to directory, where processed media will be stored."),
+    required=False,
 )
 @click.option(
     "-e",
     "--force-erase",
     is_flag=True,
-    help="Erase files after exporting them to target directory",
+    help="Erase media files after exporting them to target directory.",
 )
-def run_import(db_path, log_directory, data_directory, force_erase=False):
-    LOG.debug(db_path, log_directory, data_directory, force_erase)
+@click.option(
+    "-v",
+    "--verbose",
+    is_flag=True,
+    help="Be more verbose when logging errors.",
+)
+def run_import(
+    chat_files,
+    db_path,
+    data_directory,
+    output_directory,
+    force_erase=False,
+    verbose=False,
+):
+    """
+    Import one file or a directory CHAT_FILES into an SQLite3 database at
+    DB_PATH.
+
+    Files containing chat logs must consist of one file per chat log. Its
+    filename must match the pattern "WhatsApp Chat with <name>.txt"
+    """
+    logging.basicConfig(format="%(message)s")
+    logger = logging.getLogger(__name__)
+    logger.debug(
+        "chats path: %s, db path: %s, data dir: %s", chat_files, db_path, data_directory
+    )
+    if pathlib.Path(db_path).exists():
+        logger.warning("Database file at %s already exists! Creating backup.")
+        try:
+            shutil.copy2(db_path, f"{db_path}.{time.time()}.bkp")
+        except OSError as error:
+            logger.error("Cannot write backup database file: %s", str(error))
+            raise click.ClickException(
+                "Database file already exists, cannot write backup file."
+            )
     db = sqlite_utils.Database(db_path)
-    files = utils.crawl_directory_for_rooms(log_directory)
+    # TODO(skowalak): check if database already has correct schema. if not,
+    # initialize tables
+
+    errors = False
+    files = utils.crawl_directory_for_rooms(chat_files)
     for file in files:
-        room = utils.parse_room_file(file)
-    if force_erase:
+        try:
+            room = utils.parse_room_file(file)
+        except Exception as error:
+            logger.warning("Uncaught exception during parsing: %s", str(error))
+            errors = True
+    if force_erase and not errors:
         # TODO(skowalak): save files that have been successfully imported so we
         # don't erase rooms that failed with errors.
-        LOG.info("Erasing imported data")
+        # TODO(skowalak): Maybe populate a table with filenames and hashes?
+        logger.info("Erasing imported data")
 
-
-def read_files(filenames, db):
-    for filename in filenames:
-        read_file(filename, db)
-
-
-def read_file(filename, db):
-    LOG.info("File: ", filename)
-    messages = utils.parse(utils.tokenize(filename))
-    for message in messages:
-        utils.store_message(message, db)
-        utils.store_ids(0)
+    if errors:
+        print(
+            (
+                "Warning: Errors occurred during import. No files were deleted."
+                "Check the logs for more info, and try to run again with the -v"
+                "option"
+            )
+        )
